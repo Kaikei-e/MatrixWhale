134d4413710f1b64734623e3d0c25e274b2a9614
Kaikei-e <e_kaikei@protonmail.com>
2024-09-21 01:13:08 +0900

    Update page layout with Matrix Whale background image, headings, and dive button

--- web/app/src/routes/+page.svelte
+++ web/app/src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<div class="h-screen w-screen p-8 bg-[url('image/whale1.jpg')] bg-cover">
-	<h1 class="text-white font-semibold text-3xl font-serif">Matrix Whale</h1>
-	<h3 class=" text-white font-normal text-xl font-serif">Scale, Process, Govern</h3>
+<div
+	class="flex h-screen w-screen flex-col place-content-between bg-[url('image/whale1.jpg')] bg-cover p-8"
+>
+	<div>
+		<h1 class="font-serif text-3xl font-semibold text-white">Matrix Whale</h1>
+		<h3 class=" font-serif text-xl font-normal text-white">Scale, Process, Govern</h3>
+	</div>
+	<div class="flex w-40 justify-center">
+		<a href="/home" class="w-32 rounded-md bg-white p-2 text-center font-mono text-black">Dive</a>
+	</div>
 </div>
