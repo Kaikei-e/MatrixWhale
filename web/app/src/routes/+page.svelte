06b2e4613b421937e31468af9e028a6baad0ca40
Kaikei-e <e_kaikei@protonmail.com>
2024-09-21 09:36:46 +0900

    Update page layout with adjusted Matrix Whale heading sizes

--- web/app/src/routes/+page.svelte
+++ web/app/src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	class="flex h-screen w-screen flex-col place-content-between bg-[url('image/whale1.jpg')] bg-cover p-8"
 >
 	<div>
-		<h1 class="font-serif text-3xl font-semibold text-white">Matrix Whale</h1>
-		<h3 class=" font-serif text-xl font-normal text-white">Scale, Process, Govern</h3>
+		<h1 class="font-serif text-4xl font-semibold text-white">Matrix Whale</h1>
+		<h3 class=" font-serif text-2xl font-normal text-white">Scale, Process, Govern</h3>
 	</div>
 	<div class="flex w-40 justify-center">
 		<a href="/home" class="w-32 rounded-md bg-white p-2 text-center font-mono text-black">Dive</a>
