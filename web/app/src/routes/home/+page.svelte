62d04548533e1eff89689a994f08c9b09ecbd7ad
Kaikei-e <e_kaikei@protonmail.com>
2024-09-21 09:57:18 +0900

    Update page layout with adjusted Matrix Whale heading sizes

---
+++ web/app/src/routes/home/+page.svelte
@@ -0,0 +1,6 @@
+<div class="h-screen w-screen p-8 bg-gradient-to-r from-indigo-600 to-blue-900">
+	<div class="absolute inset-0 bg-gradient-to-r from-white to-gray-500 opacity-20"></div>
+	<div class="w-1/6">
+    <p class="text-white text-2xl">Home</p>
+  </div>
+</div>
