1df574c0bc2e43dcc57ef86b3cbb7eefab0d4369
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 18:25:23 +0900

    Update home page layout with adjusted Matrix Whale heading sizes and added grid layout

--- web/app/src/routes/home/+page.svelte
+++ web/app/src/routes/home/+page.svelte
@@ -5,10 +5,10 @@
 	</div>
 
 	<div class="... grid grid-flow-row-dense grid-cols-3 grid-rows-3">
-		<div class="col-span-2">01</div>
-		<div class="col-span-2">02</div>
-		<div>03</div>
-		<div>04</div>
-		<div>05</div>
+		<div class="col-span-2 bg-white">01</div>
+		<div class="col-span-2 bg-white">02</div>
+		<div class="bg-black">03</div>
+		<div class="bg-black">04</div>
+		<div class="bg-white">05</div>
 	</div>
 </div>
