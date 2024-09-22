a7844e22982cfda6b2dd8ca0dad42436207f5eab
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 18:33:31 +0900

    Update home page layout with adjusted grid and card styles

--- web/app/src/routes/home/+page.svelte
+++ web/app/src/routes/home/+page.svelte
@@ -1,14 +1,13 @@
 <div class="h-screen w-screen bg-gradient-to-r from-indigo-600 to-blue-900 p-8">
 	<div class="absolute inset-0 bg-gradient-to-r from-white to-gray-500 opacity-20"></div>
 	<div class="w-1/6">
-		<p class="text-2xl text-white">Home</p>
+		<p class="text-2xl text-white">Stats</p>
 	</div>
-
-	<div class="... grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
-		<div class="col-span-2 bg-white rounded-lg border border-gray-200 p-4 shadow">01</div>
-		<div class="col-span-2 bg-white rounded-lg border border-gray-200 p-4 shadow">02</div>
+	<div class="mt-8 grid h-5/6 w-full grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
+		<div class="col-span-2 rounded-lg border border-gray-200 bg-slate-300 p-4 shadow">01</div>
+		<div class="col-span-2 rounded-lg border border-gray-200 bg-slate-300 p-4 shadow">02</div>
 		<div class="rounded-lg border border-gray-200 p-4 shadow">03</div>
 		<div class="bg-black">04</div>
-		<div class="bg-white">05</div>
+		<div class="bg-slate-300">05</div>
 	</div>
 </div>
