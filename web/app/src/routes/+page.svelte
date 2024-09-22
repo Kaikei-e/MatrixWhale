1e8a44106fe3d8bb7bfe02f0cfd6880a817412d2
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 18:23:20 +0900

    Update Matrix Whale heading alignment

--- web/app/src/routes/+page.svelte
+++ web/app/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 >
 	<div>
 		<h1 class="font-serif text-4xl font-semibold text-white">Matrix Whale</h1>
-		<h3 class=" font-serif text-2xl font-normal text-white">Scale, Process, Govern</h3>
+		<h3 class="align-top font-serif text-2xl font-normal text-white">Scale, Process, Govern</h3>
 	</div>
 	<div class="flex w-40 justify-center">
 		<a href="/home" class="w-32 rounded-md bg-white p-2 text-center font-mono text-black">Dive</a>
