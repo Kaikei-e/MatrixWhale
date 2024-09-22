5b000375f7631d289fa0e357b3b7b2d261e36535
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 21:41:25 +0900

    Refactor logger initialization to use a custom LogSender struct

--- noaa_adapter/app/initialize/logger.go
+++ noaa_adapter/app/initialize/logger.go
@@ -1,11 +1,55 @@
 package initialize
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"log/slog"
-	"os"
+	"net/http"
 )
 
+type LogSender struct {
+	url string
+}
+
+func (h *LogSender) Enabled(ctx context.Context, level slog.Level) bool {
+	return true
+}
+
+func (h *LogSender) Handle(ctx context.Context, record slog.Record) error {
+	logData, err := json.Marshal(record)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", h.url, bytes.NewBuffer(logData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Encoding", "gzip")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func (h *LogSender) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *LogSender) WithGroup(name string) slog.Handler {
+	return h
+}
+
 func InitLogger() {
-	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)).With("service", "noaa_adapter"))
+	handler := &LogSender{url: "http://localhost:6000/logs"}
+
+	slog.SetDefault(slog.New(handler).With("service", "noaa_adapter"))
 	slog.Info("The Logger initialized")
 }
