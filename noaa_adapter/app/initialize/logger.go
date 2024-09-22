eec8561c6514b4cb5f55f993d0a75b8114facb56
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 20:32:16 +0900

    Refactor logger initialization in noaa_adapter

--- noaa_adapter/app/initialize/logger.go
+++ noaa_adapter/app/initialize/logger.go
@@ -6,6 +6,6 @@ import (
 )
 
 func InitLogger() {
-	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)).With("service", "noaa_adapter"))
 	slog.Info("The Logger initialized")
 }
