c2c6c0386e99a5f2db961eec4113c1e6edfe31a5
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 20:16:36 +0900

    Update main.go to initialize logger and add a goroutine for fetching NOAA alerts periodically

--- noaa_adapter/app/main.go
+++ noaa_adapter/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"noaa_adapter/adapter"
+	"noaa_adapter/initialize"
+	"sync"
+	"time"
+)
 
 func main() {
+	initialize.InitLogger()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			adapter.NoaaAlertsAdapter()
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
+	wg.Wait()
 	fmt.Println("Hello, World!")
 }
