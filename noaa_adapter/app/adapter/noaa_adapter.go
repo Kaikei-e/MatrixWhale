9cc34cee510047659872de7c4afdd788fbb9c22a
Kaikei-e <e_kaikei@protonmail.com>
2024-09-22 20:19:47 +0900

    Add NoaaAlertsAdapter function to fetch NOAA alerts from the API

---
+++ noaa_adapter/app/adapter/noaa_adapter.go
@@ -0,0 +1,44 @@
+package adapter
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+	"net/url"
+)
+
+const NoaaURL = "https://api.weather.gov"
+
+func NoaaAlertsAdapter() (string, error) {
+	targetURL, err := url.JoinPath(NoaaURL, "alerts")
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("User-Agent", "Matrix Whale, A GitHub Project")
+	req.Header.Set("Accept", "application/geo+json")
+	cl := http.Client{}
+	res, err := cl.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	slog.Info("noaa's response", "status", res.Status)
+
+	resBytes, errRead := io.ReadAll(res.Body)
+	if errRead != nil {
+		return "", errRead
+	}
+	defer res.Body.Close()
+
+	resText := string(resBytes)
+
+	slog.Info("noaa's response", "from 0 to 100 characters", resText)
+
+	return resText, nil
+}
