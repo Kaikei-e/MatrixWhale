acc87354832d8e8c8917822701f587ef5c4ab749
Kaikei-e <e_kaikei@protonmail.com>
2024-10-12 18:50:26 +0900

    Refactor sea.severity table to add area_desc column and update unique constraint

--- db/init/init.sql
+++ db/init/init.sql
@@ -6,10 +6,11 @@ CREATE SCHEMA IF NOT EXISTS sea;
 
 CREATE TABLE IF NOT EXISTS sea.severity (
   id SERIAL PRIMARY KEY,
+  area_desc VARCHAR(255) NOT NULL,
   severity VARCHAR(255) NOT NULL,
   datetime TIMESTAMP NOT NULL,
 
-  UNIQUE (severity, datetime)
+  UNIQUE (area_desc, severity)
 );
 
-CREATE INDEX idx_severity_id_datetime ON sea.severity (id, datetime);
+CREATE INDEX idx_area_desc_severity_datetime ON sea.severity (area_desc, severity, datetime);
