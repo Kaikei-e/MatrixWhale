f3cf59448ef4c5d84489c80a95277b8889e39c62
Kaikei-e <e_kaikei@protonmail.com>
2024-12-18 04:28:35 +0900

    refactor: remove NOAA area search component and related files

--- web/app/src/routes/home/+page.server.ts
+++
@@ -1,9 +0,0 @@
-// import type { PageServerLoad } from '../superforms/noaa_area/$types';
-// import type { NoaaSeverityData } from '$lib/types/noaa';
-
-// export const load = (async () => {
-// 	const noaaSeverityData: NoaaSeverityData[] = [] as NoaaSeverityData[];
-// 	return {
-// 		noaaSeverityData
-// 	};
-// }) satisfies PageServerLoad;
